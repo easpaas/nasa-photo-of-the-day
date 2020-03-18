3ef44a1a2671b81367da6c08c9c25117c409435b
evan <aspaasevan@gmail.com>
2020-03-18 12:58:43 -0700

    header component created and rendered

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/header.css';
+
+const Header = () => {
+  return (
+    <div class="header">
+      <h1>NASA <br/> Astronomy Photo Of The Day</h1>
+    </div>
+    // TODO - time permitting add a form with search option. Dependent on data from NASA API**
+  )
+}
+
+export default Header;
